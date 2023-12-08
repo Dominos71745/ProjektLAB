eb0a3c920c33fdfb526ad53855e8b487f72aef00
radzio99 <radolsaw.rudy@microsoft.wsei.edu.pl>
2023-12-08 19:52:25 +0100

    stworzenie modelu, i relacji

---
+++ Areas/Identity/Data/ApplicationDBContext.cs
@@ -0,0 +1,58 @@
+﻿using ProjektLAB.Models;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using ProjektLAB.Areas.Identity.Data;
+using static ProjektLAB.Models.Dane;
+
+namespace ProjektLAB.Areas.Identity.Data
+{
+    public class ApplicationDBContext : IdentityDbContext<ApplicationUser>
+    {
+        public ApplicationDBContext(DbContextOptions<ApplicationDBContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<Categories> Categories { get; set; }
+        public DbSet<Cars> Cars { get; set; }
+        public DbSet<Clients> Clients { get; set; }
+        public DbSet<Orders> Orders { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            base.OnModelCreating(builder);
+
+            builder.Entity<Cars>()
+          .HasOne(c => c.Category)
+          .WithMany(v => v.Cars)
+          .HasForeignKey(c => c.CategoryId);
+
+
+            builder.Entity<Orders>()
+                .HasOne(o => o.Client)
+                .WithMany(c => c.Orders)
+                .HasForeignKey(o => o.ClientId);
+
+            builder.Entity<Orders>()
+                .HasOne(o => o.Car)
+                .WithMany(c => c.Orders)
+                .HasForeignKey(o => o.CarId);
+
+            // Dodanie klucza obcego dla Cars i Clients
+            builder.Entity<Cars>().HasKey(c => new { c.CarId });
+            builder.Entity<Clients>().HasKey(c => new { c.ClientId });
+        }
+
+        private class ApplicationUserEntityConfiguration :
+            IEntityTypeConfiguration<ApplicationUser>
+        {
+            public void Configure(EntityTypeBuilder<ApplicationUser> builder)
+            {
+                builder.Property(x => x.FirstName).HasMaxLength(255);
+                builder.Property(x => x.LastName).HasMaxLength(255);
+            }
+        }
+    }
+}
--- Models/Dane.cs
+++ Models/Dane.cs
@@ -4,15 +4,57 @@ namespace ProjektLAB.Models
 {
     public class Dane
     {
-        [Required(ErrorMessage = "Prosze podaj Imie")]
-        public string Imie { get; set; }
-        [Required(ErrorMessage = "Prosze podaj Email")]
-        [RegularExpression(".+\\@.+\\.[a-z]{2,3}")]
-        public string Email { get; set; }
-        [Required(ErrorMessage = "Prosze podaj Temat")]
-        public string Temat { get; set; }
-        [Required(ErrorMessage = "Prosze podaj Tresc wiadomosci")]
-        [MinLength(10), MaxLength(50)]
-        public string Tresc { get; set; }
+        public class Categories
+        {
+            [Key]
+            public int CategoryId { get; set; }
+            public string CategoryName { get; set; }
+
+            public ICollection<Cars> Cars { get; set; }
+        }
+
+        public class Cars
+        {
+            [Key]
+            public int CarId { get; set; }
+            public string Brand { get; set; }
+            public string Model { get; set; }
+
+            public int CategoryId { get; set; }
+            public string CategoryName { get; set; }
+            public Categories Category { get; set; }
+
+            public ICollection<Categories> Categories { get; set; }
+
+            public ICollection<Orders> Orders { get; set; }
+        }
+
+        public class Clients
+        {
+            [Key]
+            public int ClientId { get; set; }
+            public string FirstName { get; set; }
+            public string LastName { get; set; }
+            public string Street { get; set; }
+            public string PostCode { get; set; }
+            public string City { get; set; }
+            public string PhoneNumber { get; set; }
+            public string Email { get; set; }
+
+            public ICollection<Orders> Orders { get; set; }
+        }
+
+        public class Orders
+        {
+            [Key]
+            public int OrderId { get; set; }
+            public DateTime OrderDate { get; set; }
+
+            public int ClientId { get; set; }
+            public Clients Client { get; set; }
+
+            public int CarId { get; set; }
+            public Cars Car { get; set; }
+        }
     }
 }
